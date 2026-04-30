9954e36d90097e0bc5aac060aa8c10036bd257f2
Ayush Sahu <shahaayush526@gmail.com>
2026-04-30 10:22:47 +0545

    Simple changes

--- Day7/API.py
+++ Day7/API.py
@@ -2,4 +2,4 @@ import requests,json
 response=requests.get("https://catfact.ninja/facts?limit=3")
 data=response.json()
 with open ("facts.json","w") as f:
-    json.dump(data,f,indent=2)#2 spaces per level
+    json.dump(data,f,indent=2)#2 spaces per level   
